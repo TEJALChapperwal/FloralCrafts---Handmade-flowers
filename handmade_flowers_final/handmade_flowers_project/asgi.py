0b10e0ee31d3ccb1a64020889f53e2139f431b63
TEJALChapperwal <tejalchhaperwal3011@gmail.com>
2025-10-02 14:42:52 +0530

    Delete handmade_flowers_final directory

--- handmade_flowers_final/handmade_flowers_project/asgi.py
+++
@@ -1,4 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handmade_flowers_project.settings')
-application = get_asgi_application()
