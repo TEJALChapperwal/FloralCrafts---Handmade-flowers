05851b8c62539caa47776d8769447b4b72783091
TEJALChapperwal <tejalchhaperwal3011@gmail.com>
2025-10-02 14:38:01 +0530

    Add files via upload

---
+++ handmade_flowers_final/handmade_flowers_final/shop/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from shop import views
+
+app_name = "shop"
+
+urlpatterns = [
+    path("", views.product_list, name="product_list"),
+
+    
+
+    # Cart
+    path("cart/", views.cart, name="cart"),
+    path("add-to-cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("order-success/", views.order_success, name="order_success"), 
+
+    # Auth
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+]
